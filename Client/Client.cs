f9717c95979784385ab262eb8b2e7d309158c34a
badralboushy <badralboushy.scholar@gmail.com>
2023-11-08 23:20:21 +0300

    uploading the project to github

---
+++ Client/Client.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+/*
+ * 
+ * author : badr
+ * 
+ * 
+ */
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            //var client = (Socket)socket;
+            //StreamReader sr = new StreamReader(path);
+            //var tosend = sr.ReadLine();
+            //tosend = tosend + "\n";
+            //while (tosend != null)
+                TcpClient client = new TcpClient();
+            client.Connect("localhost", 4000);
+            if (client.Connected)
+            {
+                Console.WriteLine( "Enter path: ");
+                byte[] buffer = new byte[1024 * 1000];
+                //var client = (Socket)socket;
+                //StreamReader sr = new StreamReader(path);
+                //var tosend = sr.ReadLine();
+                //tosend = tosend + "\n";
+                string path = Console.ReadLine();
+                
+                var stream = client.GetStream();
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                int length = BitConverter.ToInt32(buffer, 0);
+
+                string name = Encoding.ASCII.GetString(buffer, 6, length);
+                //var serverMessage = Encoding.ASCII.GetBytes(tosend);
+                //client.Send(serverMessage);
+                //tosend = sr.ReadLine();
+                string add = String.Empty;
+                for(int i=1; i<=10000; i++)
+                {
+                    add += Encoding.ASCII.GetString(buffer, 6 + (19 * (i-1)) , 19);
+                }
+                var fileWriter = new BinaryWriter(File.Open(path + name, FileMode.Append));
+                fileWriter.Write(buffer, 6 + length, bytes - 6 - length);
+                //client.Send(Encoding.ASCII.GetBytes("bye"));
+
+                stream.Close();
+                client.Close();
+                Console.WriteLine(" ************************* FIN *************************************");
+                string a;
+                a = Console.ReadLine();
+               
+            }
+
+            //    while (true)
+            //    {
+            //        byte[] recBytes = new byte[2];
+            //        int length = client.Receive(recBytes);
+            //        if (length >= 1)
+            //        {
+            //            var message = Encoding.ASCII.GetString(recBytes, 0, length);
+            //            Console.ForegroundColor = color;
+            //            Console.WriteLine($"Client:" + message);
+            //            var serverMessage = Encoding.ASCII.GetBytes(message);
+            //            client.Send(serverMessage);
+            //            if (message == "bye")
+            //            {
+            //                _clients--;
+            //                Console.ForegroundColor = color;
+            //                Console.WriteLine("Client Left");
+            //                Console.WriteLine("Remaining Client: " + _clients);
+            //                client.Close();
+            //                break;
+            //            }
+            //        }
+            //    }
+
+
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+/*
+ * 
+ * author : badr
+ * 
+ * 
+ */
+namespace Server
+{
+    class Server
+    {
+        static int _clients;
+
+        static void Main(string[] args)
+        {
+            var serverFile = "Server.txt";
+            File.Delete(serverFile);
+            string s = "HI this is a test \n";
+            string line = String.Empty;
+            Console.WriteLine("server preparing the file ..");
+            for(int i=0; i<100; i++)
+            {
+                line += s;
+
+            }
+            for (int i = 0; i < 100; i++)
+                File.AppendAllText(serverFile, line);
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(ip, 4000);
+            server.Start();
+            Console.WriteLine("Server Started");
+            ConsoleColor[] colors = new ConsoleColor[5]
+            {
+                ConsoleColor.Blue,
+                ConsoleColor.Cyan,
+                ConsoleColor.Yellow,
+                ConsoleColor.Magenta,
+                ConsoleColor.Green
+            };
+            while (true)
+            {
+                var recClient = server.AcceptSocket();
+                _clients++;
+                Console.WriteLine("New Client Joined\n\n\n");
+                new Thread(() =>
+                {
+                    ServeClient(recClient, serverFile, colors[_clients % 5]);
+                    
+
+                }).Start();
+            }
+        }
+        static void ServeClient(Socket client, string file, ConsoleColor color)
+        {
+            try
+            {
+                Console.WriteLine("************start*************");
+                byte[] tosend = Encoding.ASCII.GetBytes(file);
+                byte[] content = File.ReadAllBytes(file);
+
+                byte[] whatosend = new byte[6 + tosend.Length + content.Length];
+
+                byte[] fileNameLen = BitConverter.GetBytes(tosend.Length);
+                fileNameLen.CopyTo(whatosend, 0);
+                    tosend.CopyTo(whatosend, 6);
+                content.CopyTo(whatosend, 6 + tosend.Length);
+                client.Send(whatosend);
+                Console.WriteLine("************it's ended   ***********");
+                //sr.Close();
+                //client.Close();
+                client.Close();
+
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+           
+        }
+    }
+}
